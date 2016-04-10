9f1960300438abc32d070fe51c9d7aa60aae5c28
Matt Bishop <cmbishop@pdx.edu>
2016-04-09 21:33:00 -0700

    Included create and get function for app.js; beginning work on encryption

--- password_manager/app.js
+++ password_manager/app.js
@@ -3,6 +3,51 @@ console.log('starting password manager');
 var storage = require('node-persist');
 storage.initSync();
 
+//create
+// --name
+// --username
+// --password
+
+// get
+// --name
+var argv = require('yargs')
+    .command('create', 'Create a new account', function(yargs) {
+        yargs.options({
+            name: {
+                demand: true,
+                alias: 'n',
+                description: 'Account name (eg: Twitter, Facebook)',
+                type: 'string'
+            },
+            username: {
+                demand: true,
+                alias: 'u',
+                description: 'Account username or email',
+                type: 'string'
+            },
+            password: {
+                demand: true,
+                alias: 'p',
+                description: 'Account password',
+                type: 'string'
+            }
+        }).help('help');
+    })
+    .command('get', 'Get an existing account', function(yargs) {
+        yargs.options({
+            name: {
+                demand: true,
+                alias: 'n',
+                description: 'Account name (eg: Twitter, Facebook)',
+                type: 'string'
+            }
+        }).help('help');
+    })
+    .help('help')
+    .argv;
+
+var command = argv._[0];
+
 //account.name
 //account.username
 //account.password
@@ -34,14 +79,24 @@ function getAccount (accountName) {
 
 }
 
-//testing
+if(command === 'create') {
+    var createdAccount = createAccount({
+        name: argv.name,
+        username: argv.username,
+        password: argv.password
+    });
+    console.log('Account created!');
+    console.log(createdAccount);
+} else if(command === 'get') {
+    var fetchedAccount = getAccount(argv.name);
 
-// createAccount({
-// 	name: 'Matt',
-// 	username: 'matt@email.com',
-// 	password: '1234'
-// });
+    if(typeof fetchedAccount === 'undefined') {
+        console.log('Account not found');
+    } else {
+        console.log('Account found!');
+        console.log(fetchedAccount);
+    }
+}
 
-// var mattAccount = getAccount('Matt');
-// console.log(mattAccount);
+
 
--- password_manager/example-args.js
+++ password_manager/example-args.js
@@ -1,4 +1,22 @@
-var argv = require('yargs').argv;
+var argv = require('yargs')
+		.command('hello', 'Greets the user', function(yargs) {
+			yargs.options({
+				name: {
+					demand: true,
+					alias: 'n',
+					description: 'Your first name goes here',
+					type: 'string'
+				},
+				lastname: {
+					demand: true,
+					alias: 'l',
+					description: 'Your last name goes here',
+					type: 'string'
+				}
+			}).help('help');
+		})
+		.help('help')
+		.argv;
 var command = argv._[0];
 
 console.log(argv);
